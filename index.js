cb692dd8532ad7b463ec21015e7420a36f1fe853
mdg0410 <levoyersteven@gmail.com>
2023-05-04 09:41:41 -0500

    Add library Luxon & Add navigate method

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+import { DateTime } from './modules/luxon.js';
+import Router from './modules/router.js';
+
+const routerLinks = document.querySelectorAll('[data-to]');
 const todayContainer = document.getElementById('today');
 
-todayContainer.innerHTML = 'Loading///...';
+todayContainer.textContent = DateTime.now().toFormat('DDD, tt');
+
+const router = new Router('list');
+
+routerLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    router.navigate(link.dataset.to);
+  });
+});
